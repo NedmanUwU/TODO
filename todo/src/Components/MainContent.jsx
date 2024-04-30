41f74da1afebf191f51b5231c90adeeb58e99e1b
NedmanUwU <96993132+NedmanUwU@users.noreply.github.com>
2024-04-30 14:07:25 +0200

    only localstorage left to fix

--- todo/src/Components/MainContent.jsx
+++ todo/src/Components/MainContent.jsx
@@ -4,12 +4,14 @@ import Nook from '../Assets/Icons/Nook.gif'
 const MainContent = () => {
   return (
     <div className='Wrapper'>
+      <div className='Content'>
         <h2>Welcome to Nook's Todo-App!</h2>
         <img src={Nook} style={{width: '500px', borderRadius: '8px'}}/>
         <p>Now, I understand the <b>joy</b> of a peaceful day at home, but it's time 
           to stir up some motivation and get to work! <br /><br />There's nothing 
           quite like the satisfaction of checking tasks off your list and 
           making progress towards <br />your <b>goals.</b></p>
+      </div>
     </div>
 
   )
